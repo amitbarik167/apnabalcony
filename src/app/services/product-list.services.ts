9941fe3b2493795ad826b67067d0df69c47c0cf2
Amit Barik <barikamitster@gmail.com>
2021-08-17 23:30:41 -0600

    08172021-email setup

--- src/app/services/product-list.services.ts
+++ src/app/services/product-list.services.ts
@@ -20,8 +20,8 @@ export class ProductListService {
  
 
  public constructor(private httpClient: HttpClient) { 
-  this.nodeServer = 'http://localhost:3000' //Use this when running locally
- //this.nodeServer = 'https://apnabalconyapi.azurewebsites.net'; // use this when deploying to Azure App service
+ // this.nodeServer = 'http://localhost:3000' //Use this when running locally
+ this.nodeServer = 'https://apnabalconyapi.azurewebsites.net'; // use this when deploying to Azure App service
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
