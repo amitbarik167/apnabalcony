7bb8fa1d6730591bf2038be5ec8747de5289c48d
Amit Barik <barikamitster@gmail.com>
2021-08-21 11:17:52 -0600

    08212021-email functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL:'https://apnabalconyapi.azurewebsites.net'
+  API_URL:'https://apnabalconyapi.azurewebsites.net',
+  TO_EMAIL:'sales@apnabalcony.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  API_URL:'http://localhost:3000'
+  API_URL:'http://localhost:3000',
+  TO_EMAIL:'barikamitster@gmail.com;apnabalcony@gmail.com'
 };
 
 /*
