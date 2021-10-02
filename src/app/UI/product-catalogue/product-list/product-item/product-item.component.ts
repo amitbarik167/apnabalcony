a3521ac04f086e0ac7a199cf5dc5088380ed0b81
Amit Barik <barikamitster@gmail.com>
2021-10-01 23:51:34 -0600

    10012021-Cart UI changes

--- src/app/UI/product-catalogue/product-list/product-item/product-item.component.ts
+++ src/app/UI/product-catalogue/product-list/product-item/product-item.component.ts
@@ -11,10 +11,24 @@ import { Router } from '@angular/router';
 })
 export class ProductItemComponent implements OnInit {
   @Input() productItem: Product
-
-  constructor(private msgService: MessengerService, private router: Router,) { }
+  cartItems = [] as any;
+  cartTotal = 0;
+  constructor(private msgService: MessengerService, private router: Router,) 
+  {
+    this.cartItems.length = 0;
+   }
 
   ngOnInit(): void {
+    
+    if (localStorage.getItem('cart') != "") {
+      this.cartItems = JSON.parse(localStorage.getItem('cart') || "[]")
+    }
+    if (localStorage.getItem('cartTotal') != "") {
+      this.cartTotal = JSON.parse(localStorage.getItem('cartTotal') || "[]")
+    }
+    this.msgService.getCartDetails().subscribe((product: any) => {
+      this.addProductToCart(product)
+    })
 
   }
 
@@ -25,6 +39,44 @@ export class ProductItemComponent implements OnInit {
 
   itemDetails(_id: string) {
     this.router.navigate(['/productitemdetails', { productId: _id }]);
+  }
+
+  addProductToCart(product: any) {
+
+    let productExists = false
+
+    for (let i in this.cartItems) {
+      if (this.cartItems[i].id === product._id) {
+        this.cartItems[i].qty++
+        productExists = true
+        this.recalculateTotalPrice()
+        localStorage.setItem('cart', JSON.stringify(this.cartItems))
+        return;
+      }
+    }
+
+    if (!productExists) {
+      this.cartItems.push({
+        id: product._id,
+        discount: product.productDiscount,
+        productName: product.productName,
+        qty: 1,
+        price: product.productPrice,
+        productImg: product.productImg
+      })
+
+      this.recalculateTotalPrice()
+      localStorage.setItem('cart', JSON.stringify(this.cartItems))
+    }
+  }
+
+  recalculateTotalPrice() {
+    this.cartTotal = 0
+    this.cartItems.forEach((item: any) => {
+      this.cartTotal += (item.qty * (item.price * (100 - (item.discount)) / 100))
+      localStorage.removeItem('cartTotal');
+      localStorage.setItem('cartTotal', JSON.stringify(this.cartTotal))
+    })
   }
 
 }
